df6e22ef295449cbc0461dd1b0d17e48f566e7d9
kim <kim@DESKTOP-043HJO1>
2021-02-23 18:26:26 +0900

    10872번,팩토리얼

---
+++ baekjoon/src/baekjoon/test10872.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class test10872 {
+ 
+	public static void main(String[] args) throws IOException {
+		 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int sum = factorial(N);
+		System.out.println(sum);
+		
+	}
+	
+	public static int factorial(int N) {
+		if(N <= 1) return 1;	// 재귀 종료조건
+		return N * factorial(N - 1);		
+	}
+ 
+}
