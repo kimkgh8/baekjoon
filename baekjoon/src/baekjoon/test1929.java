54194b9791bffe6d0dd9befc6bf24c7c48a9c215
KimGuenho <kimkgh8@gmail.com>
2021-02-27 22:01:48 +0900

    1929번,소수 구하기(연습용 포함)

--- baekjoon/src/baekjoon/test1929.java
+++ baekjoon/src/baekjoon/test1929.java
@@ -2,14 +2,19 @@ package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.io.IOException;
  
-public class test11653 {
+public class test1929 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
 		StringBuilder sb = new StringBuilder();
-		for (int i=2;i*i<=N;i++) {
+		
+		for (int i=M;i*i<=N;i++) {
 			while (N % i == 0) {
 				sb.append(i).append("\n");
 				N /= i;
