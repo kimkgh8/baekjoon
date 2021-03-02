da2a837e7219e7c378ef98dc99853641b31296c8
KimGuenho <kimkgh8@gmail.com>
2021-03-02 20:14:05 +0900

    1085번,직사각형에서 탈출

---
+++ baekjoon/src/baekjoon/test1085.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+ 
+public class test1085 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		
+		int x_min = Math.min(x, w-x); // x축 최소거리
+		int y_min = Math.min(y, h-y); // y축 최소거리
+
+		System.out.println(Math.min(x_min, y_min));
+	}
+
+}
+ 
