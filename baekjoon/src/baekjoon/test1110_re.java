1371a9305b670b623d6051dc4db7cc951db59281
KimGuenho <kimkgh8@gmail.com>
2021-04-04 17:30:53 +0900

    주어진 수가 1일경우 에러 발생.

---
+++ baekjoon/src/baekjoon/test1110_re.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class test1110_re {
+ 
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	String n = br.readLine();
+    	int cnt = 0;
+    	int sum = 0;
+    	String newN = n;
+    	while (true) {
+    		cnt++;
+    		int start = Integer.parseInt(newN.substring(0,1));
+    		int end = Integer.parseInt(newN.substring(1,newN.length()));
+    		sum = start + end;
+			String sumString = String.valueOf(sum);
+			// 자릿수가1이면
+			if (sumString.length() == 1) {
+				newN =  String.valueOf(end) + sumString;
+			} else {
+				newN = String.valueOf(end) + sumString.substring(1,sumString.length());
+			}
+			if (newN.equals(n)) {
+				break;
+			}
+    	}
+    	System.out.println(cnt);
+    	
+    }
+}	
