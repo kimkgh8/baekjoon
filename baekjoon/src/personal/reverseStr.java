50080910debb03ccd659921a22dc2baef4e6577e
KimGuenho <kimkgh8@gmail.com>
2021-04-26 21:22:43 +0900

    문자열 뒤집기

---
+++ baekjoon/src/personal/reverseStr.java
@@ -0,0 +1,14 @@
+package personal;
+
+public class reverseStr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String target = "String";
+		StringBuilder sb = new StringBuilder();
+		sb.append(target);
+		sb.reverse();
+		System.out.println(sb);
+	}
+
+}
