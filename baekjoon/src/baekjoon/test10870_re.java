1726cb0ab41360860dcf87421eb680bc4bd064cb
KimGuenho <kimkgh8@gmail.com>
2021-04-27 23:56:48 +0900

    피보나치 복습

---
+++ baekjoon/src/baekjoon/test10870_re.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class test10870_re {
+ 
+	public static void main(String[] args) throws IOException {
+		 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int sum = fibonacci(N);
+		System.out.println(sum);
+		
+	}
+	
+	public static int fibonacci(int N) {
+		if(N == 0) return 0;	
+		if(N == 1) return 1;
+		return fibonacci(N - 1) + fibonacci(N - 2);		
+	}
+ 
+}
