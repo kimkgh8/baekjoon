ae49cc763936486553c211b235df8708b52ffaa8
KimGuenho <kimkgh8@gmail.com>
2021-05-17 22:24:19 +0900

    다시한번

--- baekjoon/src/baekjoon/test11399_re.java
+++ baekjoon/src/baekjoon/test11399_re.java
@@ -3,30 +3,28 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class test11399_re {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int N = Integer.parseInt(br.readLine());
 		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
 		int[] arr = new int[1001];
+		StringTokenizer st= new StringTokenizer(br.readLine()," ");
 		
-		//카운팅정렬
-		while(N-- > 0) {
+		while(N-- > 0 ) {
 			arr[Integer.parseInt(st.nextToken())]++;
 		}
 		
 		int prev = 0;
-		int sum = 0;
+		int sum =0;
 		
 		for (int i=0;i<1001;i++) {
-			while (arr[i]-- > 0 ) {
-				sum += (i+prev);
+			
+			while(arr[i]-- >0) {
+				sum += (i + prev);
 				prev += i;
 			}
 		}
