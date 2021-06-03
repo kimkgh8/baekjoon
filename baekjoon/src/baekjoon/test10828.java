07b404f606d4abaf5a62f03812a6a3c14809731f
KimGuenho <kimkgh8@gmail.com>
2021-06-03 23:31:24 +0900

    스택 중간

---
+++ baekjoon/src/baekjoon/test10828.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+ 
+public class test10828 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st;
+		
+		int N =Integer.parseInt(br.readLine());
+		for (int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+		}
+		
+	}
+	
+}
+ 
