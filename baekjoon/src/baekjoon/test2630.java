5f351b762111920fd7938b2a1be47926efba24d9
KimGuenho <kimkgh8@gmail.com>
2021-06-21 23:00:29 +0900

    복습

--- baekjoon/src/baekjoon/test2630.java
+++ baekjoon/src/baekjoon/test2630.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class test2630 {
-	// 색상 카운트 할 변수 및 색종이(board)
 	public static int white = 0;
 	public static int blue = 0;
 	public static int[][] board;
@@ -27,7 +26,6 @@ public class test2630 {
 				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
 		partition(0, 0, N);
 
 		System.out.println(white);
@@ -46,28 +44,22 @@ public class test2630 {
 			return;
 		}
 
-		int newSize = size / 2; // 절반 사이즈
-		// 재귀 호출
-		partition(row, col, newSize); // 2사분면
-		partition(row, col + newSize, newSize); // 1사분면
-		partition(row + newSize, col, newSize); // 3사분면
-		partition(row + newSize, col + newSize, newSize); // 4사분면
+		int newSize = size / 2; 
+		partition(row, col, newSize); 
+		partition(row, col + newSize, newSize); 
+		partition(row + newSize, col, newSize); 
+		partition(row + newSize, col + newSize, newSize);
 	}
 
-	// 현재 파티션의 컬러가 같은지 체크한다.
 	public static boolean colorCheck(int row, int col, int size) {
-
 		int color = board[row][col]; // 첫 번째 원소를 기준으로 검사
-
 		for (int i = row; i < row + size; i++) {
 			for (int j = col; j < col + size; j++) {
-				// 색상이 같이 않다면 false를 리턴
 				if (board[i][j] != color) {
 					return false;
 				}
 			}
 		}
-		// 검사가 모두 통과했다는 의미이므로 true 리턴
 		return true;
 	}
 }
